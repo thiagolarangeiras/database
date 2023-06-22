319b4ad35991b772a545761a8dd286e205be710d
Thiago larangeira <69261880+thiagolarangeiras@users.noreply.github.com>
2023-06-21 21:30:13 -0300

    aaa

---
+++ estoque/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO categoria (nome, descricao)
+VALUES 
+    ('quimicos', 'produtos quimicos'),
+    ('metais', 'produtos metalicos'),
+    ('plastico', 'produtos plastcos'),
+    ('tecido', 'produtos de tecidos'),
+    ('comida', 'produtos de comida'),
+    ('eletronicos', 'produtos eletronicos'),
+    ('racao', 'produtos de racao'),
+    ('brinquedo da china', 'produtos da china'),
+    ('remedios', 'produtos farmaceuticos'),
+    ('hot wheels', 'produtos da mattel')
+
+INSERT INTO produto (idcategoria, nome, descricao, peso, preco)
+VALUES 
+    (1,'Cloreto de amonia', 'alguma coisa ai', 1.5, 220),
+    (2, 'Cano de metal', 'aço', 20.176, 28,71),
+    (3, 'Torneira', 'Com resistencia', 0.5, 100),
+    (4, 'toalha', 'com bordado', 0.3, 20),
+    (5, 'feijão enlatado', 'vencido', 1, 20),
+    (6, 'eletronicos', 'produtos eletronicos'),
+    ('racao', 'produtos de racao'),
+    ('brinquedo da china', 'produtos da china'),
+    ('remedios', 'produtos farmaceuticos'),
+    ('hot wheels', 'produtos da rasbro')
