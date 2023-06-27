36c2995481c65911ddf28a795bf8bcd00e53ac70
Thiago larangeira <69261880+thiagolarangeiras@users.noreply.github.com>
2023-06-26 23:14:38 -0300

    ortografia

--- estoque/insert.sql
+++ estoque/insert.sql
@@ -1,25 +1,25 @@
 INSERT INTO categoria (nome, descricao)
 VALUES 
-    ('quimicos', 'produtos quimicos'),
-    ('metais', 'produtos metalicos'),
-    ('plastico', 'produtos plastcos'),
+    ('químicos', 'produtos químicos'),
+    ('metais', 'produtos metálicos'),
+    ('plástico', 'produtos plásticos'),
     ('tecido', 'produtos de tecidos'),
     ('comida', 'produtos de comida'),
-    ('eletronicos', 'produtos eletronicos'),
-    ('racao', 'produtos de racao'),
-    ('brinquedo da china', 'produtos da china'),
-    ('remedios', 'produtos farmaceuticos'),
+    ('eletrônicos', 'produtos eletrônicos'),
+    ('ração', 'produtos de ração'),
+    ('brinquedo da China', 'produtos da China'),
+    ('remédios', 'produtos farmacêuticos'),
     ('hot wheels', 'produtos da mattel')
 
 INSERT INTO produto (idcategoria, nome, descricao, peso, preco)
 VALUES 
-    (1,'Cloreto de amonia', 'alguma coisa ai', 1.5, 220),
+    (1,'Cloreto de amônia', 'alguma coisa ai', 1.5, 220),
     (2, 'Cano de metal', 'aço', 20.176, 28,71),
-    (3, 'Torneira', 'Com resistencia', 0.5, 100),
+    (3, 'Torneira', 'Com resistência', 0.5, 100),
     (4, 'toalha', 'com bordado', 0.3, 20),
     (5, 'feijão enlatado', 'vencido', 1, 20),
-    (6, 'eletronicos', 'produtos eletronicos'),
-    ('racao', 'produtos de racao'),
-    ('brinquedo da china', 'produtos da china'),
-    ('remedios', 'produtos farmaceuticos'),
-    ('hot wheels', 'produtos da rasbro')
+    (6, 'eletrônicos', 'produtos eletrônicos'),
+    ('ração', 'produtos de ração'),
+    ('brinquedo da China', 'produtos da China'),
+    ('remédios', 'produtos farmacêuticos'),
+    ('hot wheels', 'produtos da mattel')
